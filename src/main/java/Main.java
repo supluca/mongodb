8dabe7220e1220821a6d45b94a19b13f5a884a84
supluca <luca.peter@gmx.ch>
2022-12-26 17:36:10 +0100

    3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,33 @@
+import com.mongodb.*;
+import com.mongodb.client.*;
+import com.mongodb.client.model.CreateCollectionOptions;
+import com.mongodb.client.model.CreateViewOptions;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
+import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.conversions.Bson;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("lol");
+        String mongoClientUri = "mongodb+srv://supluca-cluster.tluspsf.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority";
+
+        try(MongoClient mongoClient = MongoClients.create(mongoClientUri)) {
+            MongoDatabase database = mongoClient.getDatabase("admin");
+
+            try {
+                Bson command = new BsonDocument("ping", new BsonInt64(1));
+                Document commandResult = database.runCommand(command);
+                System.out.println("Connected successfully to server.");
+            } catch (MongoException me) {
+                System.err.println("An error occurred while attempting to run a command: " + me);
+            }
+        }
+
+
     }
 }
