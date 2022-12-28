c69c136852a36e511281b5120b8d7eb2f44eea4b
supluca <luca.peter@gmx.ch>
2022-12-28 15:49:07 +0100

    created new/own connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,45 +9,60 @@ import java.util.Iterator;
 public class Main {
 
     public static void main(String[] args) {
+        // Test 1
+        ConnectionString connectionUri = new ConnectionString("mongodb+srv://supluca2:supluca2@supluca-cluster.tluspsf.mongodb.net/?retryWrites=true&w=majority");
 
-        String mongoClientUri = "mongodb+srv://supluca2:supluca2@supluca-cluster.tluspsf.mongodb.net/?retryWrites=true&w=majority";
+        MongoClient client = MongoClients.create(
+                MongoClientSettings.builder()
+                        .applyConnectionString(connectionUri)
+                        .applicationName("baenis").build());
 
-        try(MongoClient mongoClient = MongoClients.create(mongoClientUri)) {
-            MongoDatabase database = mongoClient.getDatabase("people");
+        MongoDatabase database = client.getDatabase("people");
 
-            try {
-                Bson command = new BsonDocument("ping", new BsonInt64(1));
-                Document commandResult = database.runCommand(command);
-                System.out.println("Connected successfully to server.");
-            } catch (MongoException me) {
-                System.err.println("An error occurred while attempting to run a command: " + me);
-            }
+        MongoCollection collection = new MongoCollection() {
 
-            try {
-                MongoCollection<Document> collection = database.getCollection("people");
-//                try (MongoCursor<Document> cur = collection.find().iterator()) {
+
+
+
+        // Test 2
+//        String mongoClientUri = "mongodb+srv://supluca2:supluca2@supluca-cluster.tluspsf.mongodb.net/?retryWrites=true&w=majority";
 //
-//                    while (cur.hasNext()) {
+//        try(MongoClient mongoClient = MongoClients.create(mongoClientUri)) {
+//            MongoDatabase database = mongoClient.getDatabase("people");
 //
-//                        var doc = cur.next();
-//                        var cars = new ArrayList<>(doc.values());
+//            try {
+//                Bson command = new BsonDocument("ping", new BsonInt64(1));
+//                Document commandResult = database.runCommand(command);
+//                System.out.println("Connected successfully to server.");
+//            } catch (MongoException me) {
+//                System.err.println("An error occurred while attempting to run a command: " + me);
+//            }
 //
-//                        System.out.printf("%s: %s%n", cars.get(1), cars.get(2));
-//                    }
+//            try {
+//                MongoCollection<Document> collection = database.getCollection("people");
+////                try (MongoCursor<Document> cur = collection.find().iterator()) {
+////
+////                    while (cur.hasNext()) {
+////
+////                        var doc = cur.next();
+////                        var cars = new ArrayList<>(doc.values());
+////
+////                        System.out.printf("%s: %s%n", cars.get(1), cars.get(2));
+////                    }
+////                }
+//
+//                FindIterable<Document> allData = collection.find();
+//                Iterator it = allData.iterator();
+//                while(it.hasNext()){
+//                    System.out.println(it.next());
 //                }
-
-                FindIterable<Document> allData = collection.find();
-                Iterator it = allData.iterator();
-                while(it.hasNext()){
-                    System.out.println(it.next());
-                }
-
-
-            } catch (MongoException me) {
-                System.err.println("An error occurred while attempting to run a command: " + me);
-            }
-
-        }
+//
+//
+//            } catch (MongoException me) {
+//                System.err.println("An error occurred while attempting to run a command: " + me);
+//            }
+//
+//        }
 
     }
 }
